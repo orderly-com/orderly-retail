3fda34d56462d71b211d1d6de80dd4a2c40e092a
RayYangFromOrderly <ray.yang@ezorderly.com>
2021-11-30 18:48:08 +0800

    bf

--- src/orderly_retail/team/conditions.py
+++ src/orderly_retail/team/conditions.py
@@ -122,7 +122,7 @@ class ProductCategoryCondition(SelectCondition):
         return client_qs, q
 
     def real_time_init(self, team, *args, **kwargs):
-        category_qs = team.productcategorybase_set.values(text='name', id='uuid')
+        category_qs = team.productcategorybase_set.values(text=F('name'), id=F('uuid'))
         self.choice(*category_qs)
 
 
@@ -143,7 +143,7 @@ class ProductCondition(SelectCondition):
         return client_qs, q
 
     def real_time_init(self, team, *args, **kwargs):
-        product_qs = team.productbase_set.filter(removed=False).values(text='name', id='uuid')
+        product_qs = team.productbase_set.filter(removed=False).values(text=F('name'), id=F('uuid'))
         self.choice(*product_qs)
 
 # valuetag
